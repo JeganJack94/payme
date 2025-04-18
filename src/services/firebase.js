a175251e2f6352df67286cd25419800fd59664cd
Jeganraj Arul Alphones <jegan@Jeganrajs-Mac-mini.local>
2025-04-18 09:39:53 +0530

    Env Correction

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyChc7TveXX1TJ4NI6TgVpuu7nGnKYNPiZI",
+  authDomain: "payme-5259f.firebaseapp.com",
+  projectId: "payme-5259f",
+  storageBucket: "payme-5259f.firebasestorage.app",
+  messagingSenderId: "378246228500",
+  appId: "1:378246228500:web:4c7bdb4d7b9356ac4a9501"
 };
 
 // Initialize Firebase
